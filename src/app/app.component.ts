6e08f70a798ea36d33a6f17ce11c2f3c8f2876e6
Liem <leliem28@gmail.com>
2021-07-09 21:26:35 +0700

    Hoan thanh bai 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularBai5';
+  name: string = "";
+  isLogin: boolean = false;
+  
+  login(username: string, password: string) {
+    if (username == "cybersoft" && password == "cybersoft") {
+      localStorage.setItem('login', JSON.stringify(username));
+    }
+  }
+  
+  ngOnInit(): void {
+    this.name = JSON.parse(localStorage.getItem('login')!);
+    console.log(this.name);
+    
+  }
 }
